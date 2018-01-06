3bc47be911a543cb7ed0c0a52ffaf599d1c1103a
scottzijiezhang <scottzjzhang@uchicago.edu>
2018-01-05 18:13:16 -0600

    start

---
+++ R/PlotGene.R
@@ -0,0 +1,49 @@
+
+
+## the main function to plot m6A-seq on one group of data
+plotGene <- function(IP_BAM, INPUT_BAM, size.IP, size.INPUT, geneName, geneModel, center = mean ,ZoomIn=NULL){
+  IP.cov <- getAveCoverage(geneModel= geneModel,bamFiles = IP_BAM,geneName = geneName,size.factor = size.IP,center = center, ZoomIn = ZoomIn)
+  INPUT.cov <- getAveCoverage(geneModel= geneModel,bamFiles = INPUT_BAM,geneName = geneName,size.factor = size.INPUT, center = center,ZoomIn = ZoomIn)
+  cov.data <- data.frame(IP=IP.cov,Input=INPUT.cov,genome_location=as.numeric(names(IP.cov) ) )
+  ggplot(data = cov.data,aes(genome_location))+geom_line(aes(y=Input,colour ="Input"))+geom_line(aes(y=IP,colour="IP"))+labs(y="normalized reads")+scale_x_continuous(breaks = round(seq(min(cov.data$genome_location), max(cov.data$genome_location), by = ((max(cov.data$genome_location)-min(cov.data$genome_location))/10) ),1))
+}
+
+## the main function to plot m6A-seq on two group of data
+plotGenePair <- function(Ctl_IP_BAM,Ctl_INPUT_BAM,Treat_IP_BAM,Treat_INPUT_BAM,Ctl_size.IP,Ctl_size.INPUT,Treat_size.IP,Treat_size.INPUT,geneName,geneModel,center = mean,ZoomIn=NULL){
+  Ctl_IP.cov <- getAveCoverage(geneModel= geneModel,bamFiles = Ctl_IP_BAM,geneName = geneName,size.factor = Ctl_size.IP,center = center, ZoomIn = ZoomIn)
+  Ctl_INPUT.cov <- getAveCoverage(geneModel= geneModel,bamFiles = Ctl_INPUT_BAM,geneName = geneName,size.factor = Ctl_size.INPUT,center = center , ZoomIn = ZoomIn)
+  Treat_IP.cov <- getAveCoverage(geneModel= geneModel,bamFiles = Treat_IP_BAM,geneName = geneName,size.factor = Treat_size.IP, center = center,ZoomIn = ZoomIn)
+  Treat_INPUT.cov <- getAveCoverage(geneModel= geneModel,bamFiles = Treat_INPUT_BAM,geneName = geneName,size.factor = Treat_size.INPUT, center = center,ZoomIn = ZoomIn)
+  cov.data <- data.frame(Ctl_IP=Ctl_IP.cov, Ctl_Input = Ctl_INPUT.cov,
+                         Treat_IP=Treat_IP.cov, Treat_Input = Treat_INPUT.cov,
+                         genome_location=as.numeric(names(Ctl_IP.cov) ) )
+  ggplot(data = cov.data,aes(genome_location))+geom_line(aes(y=Ctl_Input,colour ="Ctl Input"))+geom_line(aes(y=Treat_IP,colour="Treat IP"))+geom_line(aes(y=Treat_Input,colour ="Treat Input"))+geom_line(aes(y=Ctl_IP,colour="Ctl IP"))+labs(y="normalized reads")+scale_x_continuous(breaks = round(seq(min(cov.data$genome_location), max(cov.data$genome_location), by = ((max(cov.data$genome_location)-min(cov.data$genome_location))/10) ),1))
+}
+
+## helper function to get average coverage of a gene of multiple samples
+getAveCoverage <- function(geneModel,bamFiles,geneName,size.factor, center ,ZoomIn){
+  locus <- as.data.frame( range(geneModel[geneName][[1]]) )
+  if(is.null(ZoomIn)){
+  }else{
+    locus$start = ZoomIn[1]
+    locus$end = ZoomIn[2]
+    locus$width = ZoomIn[2] - ZoomIn[1] + 1
+  }
+  covs <- sapply(bamFiles,getCov,locus=locus)
+  covs <- covs/size.factor
+  ave.cov <- apply(covs,1, center)
+  return(ave.cov)
+}
+
+getCov <- function(bf,locus){
+  s_param <- ScanBamParam(which = GRanges(locus$seqnames,IRanges(locus$start,locus$end)))
+  p_param <- PileupParam(max_depth=1000000,min_nucleotide_depth=0,distinguish_nucleotides=F,)
+  #get coverage from the bam file
+  res <- pileup(bf,scanBamParam = s_param,pileupParam = p_param)
+  res <- res[res$strand!=locus$strand,]
+  cov <- vector(length = locus$width)
+  names(cov) <- c(locus$start:locus$end)
+  cov[1:locus$width] <- 0
+  cov[res$pos-locus$start+1] <- res$count
+  return(cov)
+}
---
+++ R/peakDistribution.R
@@ -0,0 +1,63 @@
+#' @title gtfToGeneModel
+#' @description to remove ambiguous gene model and return gene model as genomic ranges object
+#' @param bedFile peak list as bed format
+#' @param gtf gtf file to build gene model
+#' @param saveName the file name to save ditribution plot
+#' @export
+
+peakDistribution <- function(bedFile,gtf,saveName = NA){
+
+  library(GenomicFeatures)
+  peak <- read.table( bedFile ,sep = "\t",header = F)
+
+  ## collapes the peak to the center
+  x <- peak
+  for(i in 1:dim(x)[1]){
+    start = x[i,2]
+    end = x[i,3]
+    blocks = x[i,10]
+    blockStart = as.numeric( unlist(strsplit(as.character(x[i,12]),",")) )
+    blockSize = as.numeric( unlist(strsplit(as.character(x[i,11]),",")) )
+    if(blocks <2){
+      x[i,2] =  x[i,3] = round(start + sum(blockSize)/2 )
+    } else{
+      pointer = 2
+      while(sum(blockSize[1:pointer]) < sum(blockSize)/2 ){pointer = pointer+1}
+      x[i,2] =  x[i,3] = round(start + blockStart[pointer] + sum(blockSize)/2 - sum(blockSize[1:(pointer-1)]) )
+    }
+    x[i,10] = 1
+  }
+
+  ################
+  gr.peak <- makeGRangesFromDataFrame(df = x,start.field = "V2",end.field = "V3",seqnames.field = "V1",strand.field = "V6")
+  txdb <- makeTxDbFromGFF(file = gtf,format = "gtf")
+
+  cds <-  cdsBy(txdb,by = "tx")
+  n.cds <- length( which(countOverlaps(gr.peak,cds)>0) )
+
+  fiveUTR <- fiveUTRsByTranscript(txdb)
+  n.fiveUTR <- length( which(countOverlaps(gr.peak,fiveUTR)>0) )
+
+  threeUTR <- threeUTRsByTranscript(txdb)
+  n.threeUTR <-  length( which(countOverlaps(gr.peak,threeUTR)>0) )
+
+  exon <- exonsBy(txdb,by = "tx")
+  all_mRNA <- unique(c(names(fiveUTR),names(threeUTR),names(cds)))
+  name_ncRNA <- setdiff(names(exon),all_mRNA)
+  ncRNA <- exon[name_ncRNA]
+  n.ncRNA <-  length( which(countOverlaps(gr.peak,ncRNA)>0) )
+
+  slices <- c(n.cds,n.fiveUTR,n.threeUTR,n.ncRNA)
+  lbls <- c("CDS","5'UTR","3'UTR", "ncRNA")
+  pct <- round(slices/sum(slices)*100)
+  lbls <- paste(lbls, pct) # add percents to labels
+  lbls <- paste(lbls,"%",sep="") # ad % to labels
+  if(is.na(saveName)){
+    pie(slices,labels = lbls, col= rainbow(4), main="Pie Chart of m6A peak distribution")
+  } else{
+    pdf(saveName)
+    pie(slices,labels = lbls, col=rainbow(4), main="Pie Chart of m6A peak distribution")
+    dev.off()
+  }
+
+}
