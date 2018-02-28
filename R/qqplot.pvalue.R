ff787bf5a7a1947950d7555fc056a0222ef029c5
scottzijiezhang <scottzjzhang@uchicago.edu>
2018-02-28 10:00:17 -0600

    update qqplot point size

--- R/qqplot.pvalue.R
+++ R/qqplot.pvalue.R
@@ -1,7 +1,9 @@
 #' @title qqplot.pvalue
 #' @param x can be a vector (p value of one group) or a list of vector (p value of multiple groups).
+#' @param pointSize The size of data points
+#' @param legendSize The size of points in the legend
 #' @export
-qqplot.pvalue <- function(x){
+qqplot.pvalue <- function(x,pointSize = 1,legendSize = 4){
   library(ggplot2)
   if(is.list(x)){
     nn<-sapply(x, length)
@@ -28,10 +30,10 @@ qqplot.pvalue <- function(x){
     pvalues <- thin$pvalues
     exp.x <- thin$exp.x
     qq.melt <- data.frame(expected=exp.x, Observed = pvalues, label = grp)
-    ggplot(data = qq.melt, aes(expected, Observed,colour = label) )+geom_point(size = 1.5)+geom_abline(slope = 1)+xlab("Expected(-log10 p-value)")+ylab("Observed(-log10 p-value)")+
+    ggplot(data = qq.melt, aes(expected, Observed,colour = label) )+geom_point(size = pointSize)+geom_abline(slope = 1)+xlab("Expected(-log10 p-value)")+ylab("Observed(-log10 p-value)")+
       theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
                          panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+
-      guides(color = guide_legend(override.aes = list(size=8)))
+      guides(color = guide_legend(override.aes = list(size=legendSize)))
 
   }else{
     expected <- -log10(seq(0,1,1/(length(x)-1 ) ))
